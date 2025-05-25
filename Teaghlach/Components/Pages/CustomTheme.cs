f2248fcb40ad6d52832952850bf6111712527209
barrymac20 <barrymac20@gmail.com>
2025-05-25 01:50:58 +0100

    Add logo for home page

---
+++ Teaghlach/Components/Pages/CustomTheme.cs
@@ -0,0 +1,17 @@
+﻿//using MudBlazor;
+
+//public static class CustomTheme
+//{
+//    public static MudTheme MyTheme = new MudTheme
+//    {
+//        ColorOptions = new()
+//        {
+//            Primary = Colors.Blue.Default,      // Or use hex: "#1976d2"
+//            Secondary = Colors.Green.Accent4,   // Customize as needed
+//        },
+//        LayoutProperties = new()
+//        {
+//            DefaultBorderRadius = "6px"
+//        }
+//    };
+//}
